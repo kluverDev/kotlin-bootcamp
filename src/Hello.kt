2763cd5941d71b905d4d210596a775ffe7237451
wizzy akor <kluverdev@gmail.com>
2020-08-20 19:32:35 -0400

    kotlin prefers expressions

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,22 +1,16 @@
 fun main(args: Array<String>) {
-    val name = "eve"
-    //prefer to use raw string instead of escape string in kotlin
-    val raw = """The kid asked, "How's it going, $name?""""
-    //multiline string is a rw string that contains a line break
-    val mem = """Dear $name, a quick reminder about the
-party we have scheduled next Tuesday at
-the 'Low Ceremony Cafe' at Noon. | Please plan to..."""
+   fun canVote(name: String, age: Int): String {
+//        var status: String
+//        if (age > 17) {
+//            status = "yes, please vote"
+//        } else {
+//            status = "nope, please come back"
+//        }
+//        return "$name, $status"
+       val status = if (age > 17) "yes, please vote" else "nope, please come back"
+       return "$name, $status"
+   }
+   println(canVote("Eve", 12))
 
-    fun createMemoFor(name: String): String {
-        if (name == "Eve") {
-            val memo = """
-                |Dear $name, a quick reminder about the
-party we have scheduled next Tuesday at
-the 'Low Ceremony Cafe' at Noon. | Please plan to...
-                """
-            return memo.trimMargin()
-        }
-        return ""
-    }
-    println(createMemoFor("Eve"))
+
 }
