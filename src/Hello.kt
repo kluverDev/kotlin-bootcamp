b7ca2389dcbb8beca84b1665f91510b00919bcec
wizzy akor <kluverdev@gmail.com>
2020-08-27 14:43:26 -0400

    named arguments

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,25 +1,11 @@
 fun main(args: Array<String>) {
-    fun max(numbers: IntArray): Int {
-        var large = Int.MIN_VALUE
-        println(large)
-        for (number in numbers) {
-            println("$number hell")
-            large = if (number > large) number else large
-            println(large)
-        }
-        return large
+    fun greet(name: String, msg: String): String = "$msg "
+    /*println(greet("Eve")) //Hello Eve*/
+    println(greet("Eve", "Howdy")) //Howdy Eve
+    /*println(greet("name"))*/
+    fun createPerson(name: String, age: Int = 1, height: Int, weight: Int) {
+        println("$name $age $height $weight")
     }
-    println(max(intArrayOf(1, 5, 2, 12, 7, 3)))
-//    //shortest func you can write in kotlin
-//    fun greet(): String {
-//        return "string"
-//    }
-//
-//    println(greet())
-    //    fun sayHello() = println("Well, hello") //kotlin infers the type as Unit.
-//    val message: String = sayHello() //ERROR
-//    fun sayHello(): Unit = println("Well, hello")
-//    val message: Unit = sayHello()
-//    println(message).toString()
-
+    //named arguments
+    createPerson(name = "Jake", age = 12, weight = 43, height = 152)
 }
